dd4050726c8728dcbbf346d5d5d35fb7f5990afb
Bryan Matsuo <bryan.matsuo@gmail.com>
2010-04-29 03:00:39 -0700

    Initial commit.

---
+++ lib/File/QuickTemplate.pm
@@ -0,0 +1,45 @@
+#!/usr/bin/env perl
+
+package QuickTemplate;
+
+use strict;
+use warnings;
+use Carp;
+
+use Class::InsideOut;
+
+our $VERSION = '0.0_1';
+
+
+return 1;
+
+__END__
+=head1 NAME
+
+QuickTemplate - simple object describing a template file
+
+=head1 VERSION
+
+Version 0.0_1
+Originally created on 04/26/10 18:49:01
+
+=head1 ABSTRACT
+
+QuickTemplate is a class and fileformat for specifying template files and
+common tasks that go into producing them (think chmod +x)
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+Bryan Matsuo (bryan.matsuo@gmail.com)
+
+=head1 BUGS
+
+=over
+
+=back
+
+=head1 COPYRIGHT
+
+=cut
---
+++ lib/File/QuickTemplate/SubstitutionTable.pm
@@ -0,0 +1,41 @@
+#!/usr/bin/env perl
+
+package QuickTemplate::SubstitutionTable;
+
+use strict;
+use warnings;
+use Carp;
+
+use Class::InsideOut;
+
+use QuickTemplate;
+
+our $VERSION = '0.01';
+
+
+return 1;
+
+__END__
+
+=head1 NAME
+
+QuickTemplate::SubstitutionTable - QuickTemplate::Generator lookup tables.
+
+=head1 VERSION
+
+Version 0.0_1
+Originally created on 04/26/10 18:49:01
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+Bryan Matsuo (bryan.matsuo@gmail.com)
+
+=head1 BUGS
+
+=over
+
+=back
+
+=head1 COPYRIGHT
